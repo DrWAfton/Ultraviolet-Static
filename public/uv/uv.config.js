18db6cb61d06a18f4119c3ac0ba5afbd494d96bd
DrWAfton <anoaha10@gmail.com>
2026-03-03 12:13:47 -0600

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,11 +1,8 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    
     bare: 'https://uv.holyubofficial.net', 
-    
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    
     handler: '/uv/uv.handler.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
