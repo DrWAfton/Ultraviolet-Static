586397411f79fdd5852812eb271e128b266c1cc3
DrWAfton <anoaha10@gmail.com>
2026-03-03 12:01:32 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,13 +2,9 @@ importScripts('https://cdn.jsdelivr.net');
 importScripts('https://cdn.jsdelivr.net');
 importScripts('https://cdn.jsdelivr.net');
 
-const sw = new UVServiceWorker({
-    prefix: '/uv/service/',
-    bare: 'https://bare.benroberts.dev', 
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode
-});
+const sw = new UVServiceWorker();
 
 self.addEventListener('fetch', (event) => {
+
     event.respondWith(sw.fetch(event));
 });
