bf94a7d29959e158c07dbde635ebc3fa4adfce70
DrWAfton <anoaha10@gmail.com>
2026-03-03 12:41:12 -0600

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://uv.holyubofficial.net', 
+    bare: 'https://uv.student-t.cc', 
     encodeUrl: (url) => btoa(url).replace(/\//g, '_').replace(/\+/g, '-').replace(/=/g, ''),
     decodeUrl: (url) => atob(url.replace(/_/g, '/').replace(/-/g, '+')),
     handler: '/uv/uv.handler.js',
